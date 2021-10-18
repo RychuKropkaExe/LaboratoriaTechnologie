e8193dda82ed86c72152a722abac84141ce10e81
RychuKropkaExe <juzek9898@o2.pl>
2021-10-18 09:19:15 +0200

    Próba wywołania konfliktu

--- src/main/java/AreaCalculator.java
+++ src/main/java/AreaCalculator.java
@@ -1,9 +1,6 @@
 public class AreaCalculator {
   public static void main(String[] args) {
-      if (true) {
-          return;
-      } else
-          return;
+      String figure = args[0];
   }
 }
 
