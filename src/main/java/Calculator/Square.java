1b951dc7309b4ac63f371f071bc84f44344ce1e7
RychuKropkaExe <juzek9898@o2.pl>
2021-10-18 09:42:29 +0200

    Określenie wszystkich parametrów klasy

--- src/main/java/Calculator/Square.java
+++ src/main/java/Calculator/Square.java
@@ -1,4 +1,11 @@
 package Calculator;
 
 public class Square {
+    double side;
+    Square(double a){
+        side = a;
+    }
+    public static double SquareArea(double a){
+        return a*a;
+    }
 }
