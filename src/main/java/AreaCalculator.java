2ddecfee614a0932ab6648b3fb3762b741a99949
RychuKropkaExe <juzek9898@o2.pl>
2021-10-18 09:06:43 +0200

    Zmiany gównianego kodu

--- src/main/java/AreaCalculator.java
+++ src/main/java/AreaCalculator.java
@@ -1,10 +1,9 @@
 public class AreaCalculator {
-  public static void main(String[] args){
-    if(true){
-    return 0;
-    }
-    else
-      return 0;
-    return 0;
+  public static void main(String[] args) {
+      if (true) {
+          return;
+      } else
+          return;
+  }
 }
 
